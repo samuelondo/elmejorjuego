cf44c822ff3eeaca63da41e42915a7fb4caf35eb
samuelondo <135150972+samuelondo@users.noreply.github.com>
2024-04-29 18:08:02 -0500

    cambios

--- chooseDevice.js
+++ chooseDevice.js
@@ -3,18 +3,5 @@ function startGame() {
     const deviceSelect = document.getElementById('deviceSelect');
     const mode = modeSelect.value;
     const device = deviceSelect.value;
-
-    if (device === 'mobile') {
-        changeOrientation();
-    }
-
     window.location.href = `game.html?mode=${mode}&device=${device}`;
-}
-
-function changeOrientation() {
-    if (screen.orientation && screen.orientation.lock) {
-        screen.orientation.lock('landscape').catch(function(error) {
-            console.warn("Screen orientation lock failed: ", error);
-        });
-    }
 }
